0c3dfe1684ac4f111b4e838526327bc500170730
BashirovAlfit <alfit.bashirov@gmail.com>
2022-04-04 22:17:43 +0600

    Initial commit

---
+++ src/main/java/com/example/java_group_11_homework_56/JavaGroup11Homework56Application.java
@@ -0,0 +1,13 @@
+package com.example.java_group_11_homework_56;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGroup11Homework56Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGroup11Homework56Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_group_11_homework_56/JavaGroup11Homework56ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_group_11_homework_56;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGroup11Homework56ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
